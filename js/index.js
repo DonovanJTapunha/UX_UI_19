c2ad864c0e93f373c9ce6e28a6b6d7f9d5fdc9d4
dondsgns <71228877+dondsgns@users.noreply.github.com>
2021-03-03 19:40:12 -0500

    UX_UI_19

---
+++ js/index.js
@@ -0,0 +1,12 @@
+console.log("Your index.js is loaded correctly! :D");
+
+
+function onHover()
+{
+    $("#logoBlack").attr('src', '../UX_UI_19/images/logoBlack.svg');
+}
+
+function offHover()
+{
+    $("#logoBlack").attr('src', 'https://celinechenu.com/wp-content/uploads/2020/04/logo.png');
+}
